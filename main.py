ffbdda452942408cac07431256f39dbe8de11a70
Misha Polunin <polunin1986@gmail.com>
2026-03-03 13:19:13 +0200

    -added docker-compose.yml - added mongodb - edited methods to work with mongodb

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import json
 import random
 import string
 from typing import Annotated
 
+import pymongo
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from starlette.responses import RedirectResponse
@@ -11,6 +11,10 @@ app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 BASE_URL = "http://127.0.0.1:8000"
+
+client = pymongo.AsyncMongoClient("mongodb://admin:password@localhost:27017")
+shorter_urls_db = client["shorter_urls_db"]
+urls_collection = shorter_urls_db["urls"]
 
 
 @app.get("/")
@@ -21,26 +25,26 @@ async def root(request: Request):
 @app.post("/")
 async def create_short_url(long_url: Annotated[str, Form()]):
     available_chars = string.ascii_letters + string.digits
+
     short_url = ''.join(random.choices(available_chars, k=7))
+    while await urls_collection.find_one({"short_url": short_url}) is not None:
+        short_url = ''.join(random.choices(available_chars, k=7))
 
-    with open("urls.json", "r") as file:
-        final_urls_dict = json.loads(file.read())
+    final_document = {"short_url": short_url, "long_url": long_url, "clicks": 0}
 
-        final_urls_dict[short_url] = long_url
-
-    with open("urls.json", "w") as file:
-        file.write(json.dumps(final_urls_dict))
+    await urls_collection.insert_one(final_document)
 
     return {"message": f"Short URL created: {long_url}, short_url: {BASE_URL}/{short_url}"}
 
 
 @app.get("/{short_url}")
 async def redirect_to_long_url(short_url: str):
-    with open("urls.json", "r") as file:
-        final_urls_dict = json.loads(file.read())
+    final_urls_dict = await urls_collection.find_one({"short_url": short_url})
 
-        final_url = final_urls_dict.get(short_url)
+    if final_urls_dict:
+        query_filter = {"short_url": short_url}
+        await urls_collection.update_one(query_filter, {"$inc": {"clicks": 1}})
+        final_url = final_urls_dict.get("long_url")
 
-        if final_url:
-            return RedirectResponse(final_url)
-        return {"message": f"Short URL not found {short_url}"}
+        return RedirectResponse(final_url)
+    return {"message": f"Short URL not found {short_url}"}
